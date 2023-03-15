54b2d1e31c68974677f22d80963fb972db3c4a03
Andy Cernera <kooshaba@gmail.com>
2023-03-15 09:18:04 -0400

    fix: no need for key migration here

--- packages/client/src/getBurnerWallet.ts
+++ packages/client/src/getBurnerWallet.ts
@@ -4,10 +4,6 @@ export const getBurnerWallet = () => {
   const urlKey = new URLSearchParams(window.location.search).get("privateKey");
   if (urlKey) return new Wallet(urlKey).privateKey;
 
-  // Migrate old burnerWallet key
-  const oldBurnerWallet = localStorage.getItem("burnerWallet");
-  if (oldBurnerWallet) localStorage.setItem("mud:burnerWallet", oldBurnerWallet);
-
   const privateKey = localStorage.getItem("mud:burnerWallet");
   if (privateKey) return privateKey;
 
