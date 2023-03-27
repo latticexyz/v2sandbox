ff2cec9e47827c57e385d12ece86d213babbe1db
alvarius <89248902+alvrs@users.noreply.github.com>
2023-03-28 01:20:38 +0200

    feat: use deploy artifact and wagmi chain config as fallback to url params (#10)
    * feat: use deployment info json for config
    
    * chore: remove deploy artifacts from github
    
    * rework
    
    ---------
    
    Co-authored-by: Kevin Ingersoll <kingersoll@gmail.com>

--- packages/client/src/config.ts
+++ packages/client/src/config.ts
@@ -1,6 +1,27 @@
 import { SetupContractConfig } from "@latticexyz/std-client";
 import { Wallet } from "ethers";
+import { foundry } from "@wagmi/chains";
+import type { Chain } from "@wagmi/chains";
+import { latticeTestnet } from "./latticeTestnet";
+import localDeploy from "../../contracts/deploys/31337/latest.json";
+
+const chains: Chain[] = [foundry, latticeTestnet];
+
 const params = new URLSearchParams(window.location.search);
+const chainId = Number(params.get("chainId") || import.meta.env.VITE_CHAIN_ID);
+if (!chainId) {
+  throw new Error("No chainId provided");
+}
+
+const chain = chains.find((c) => c.id === chainId);
+if (!chain) {
+  throw new Error(`Chain ${chainId} not found`);
+}
+
+const deploy = chain === foundry ? localDeploy : undefined;
+if (!deploy) {
+  throw new Error(`No deployment found for chain ${chainId}`);
+}
 
 export const config: SetupContractConfig = {
   clock: {
@@ -9,14 +30,16 @@ export const config: SetupContractConfig = {
     syncInterval: 5000,
   },
   provider: {
-    jsonRpcUrl: params.get("rpc") ?? "http://localhost:8545",
-    wsRpcUrl: params.get("wsRpc") ?? "ws://localhost:8545",
-    chainId: Number(params.get("chainId")) || 31337,
+    jsonRpcUrl: params.get("rpc") ?? chain.rpcUrls.default.http[0],
+    wsRpcUrl: params.get("wsRpc") ?? chain.rpcUrls.default.webSocket?.[0],
+    chainId,
   },
   privateKey: Wallet.createRandom().privateKey,
-  chainId: Number(params.get("chainId")) || 31337,
+  chainId,
   snapshotServiceUrl: params.get("snapshot") ?? undefined,
-  initialBlockNumber: Number(params.get("initialBlockNumber")) || 0,
-  worldAddress: params.get("worldAddress")!,
+  initialBlockNumber: Number(
+    params.get("initialBlockNumber") || deploy.blockNumber
+  ),
+  worldAddress: params.get("worldAddress") || deploy.worldAddress,
   devMode: params.get("dev") === "true",
 };
