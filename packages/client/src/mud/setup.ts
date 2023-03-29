1e84d72b94eed49b0903c49abb7476d3f6d40860
dk1a <dk1a@protonmail.com>
2023-03-29 22:31:05 +0300

    feat: use tsgen (#15)
    * feat: use tsgen
    
    * update tsgen
    
    * move tsgen to client
    
    ---------
    
    Co-authored-by: Kevin Ingersoll <kingersoll@gmail.com>

--- packages/client/src/mud/setup.ts
+++ packages/client/src/mud/setup.ts
@@ -1,73 +1,54 @@
-import { setupMUDNetwork } from "@latticexyz/std-client";
+import { setupMUDV2Network } from "@latticexyz/std-client";
+import { createFaucetService } from "@latticexyz/network";
 import { getNetworkConfig } from "./getNetworkConfig";
-import { contractComponents, clientComponents } from "./components";
+import { defineContractComponents } from "./contractComponents";
+import { clientComponents } from "./clientComponents";
 import { world } from "./world";
-import { EntityID } from "@latticexyz/recs";
-import { createFaucetService, SingletonID } from "@latticexyz/network";
 import { utils } from "ethers";
 
 export type SetupResult = Awaited<ReturnType<typeof setup>>;
 
 export async function setup() {
+  const contractComponents = defineContractComponents(world);
   const networkConfig = await getNetworkConfig();
-  const result = await setupMUDNetwork<typeof contractComponents, never>(
+  const result = await setupMUDV2Network<typeof contractComponents>({
     networkConfig,
     world,
     contractComponents,
-    {} as never,
-    { syncThread: "main" }
-  );
+  });
 
   result.startSync();
 
-  // For LoadingState updates
-  const singletonEntity = world.registerEntity({ id: SingletonID });
-
-  // Register player entity
-  const address = result.network.connectedAddress.get();
-  if (!address) throw new Error("Not connected");
-
-  const playerEntityId = address as EntityID;
-  const playerEntity = world.registerEntity({ id: playerEntityId });
-
-  const components = {
-    ...result.components,
-    ...clientComponents,
-  };
-
   // Request drip from faucet
-  if (!networkConfig.devMode && networkConfig.faucetServiceUrl) {
+  const signer = result.network.signer.get();
+  if (!networkConfig.devMode && networkConfig.faucetServiceUrl && signer) {
+    const address = await signer.getAddress();
+    console.info("[Dev Faucet]: Player address -> ", address);
+
     const faucet = createFaucetService(networkConfig.faucetServiceUrl);
-    console.info("[Dev Faucet]: Player Address -> ", address);
 
     const requestDrip = async () => {
-      const balance = await result.network.signer.get()?.getBalance();
-      console.info(`[Dev Faucet]: Player Balance -> ${balance}`);
-      const playerIsBroke = balance?.lte(utils.parseEther("1"));
-      console.info(`[Dev Faucet]: Player is broke -> ${playerIsBroke}`);
-      if (playerIsBroke) {
-        console.info("[Dev Faucet]: Dripping funds to player");
+      const balance = await signer.getBalance();
+      console.info(`[Dev Faucet]: Player balance -> ${balance}`);
+      const lowBalance = balance?.lte(utils.parseEther("1"));
+      if (lowBalance) {
+        console.info("[Dev Faucet]: Balance is low, dripping funds to player");
         // Double drip
-        address &&
-          (await faucet?.dripDev({ address })) &&
-          (await faucet?.dripDev({ address }));
+        await faucet.dripDev({ address });
+        await faucet.dripDev({ address });
       }
     };
 
     requestDrip();
     // Request a drip every 20 seconds
-    // TODO: tie this into tx queue instead?
     setInterval(requestDrip, 20000);
   }
 
   return {
     ...result,
-    world,
-    networkConfig,
-    singletonEntityId: SingletonID,
-    singletonEntity,
-    playerEntityId,
-    playerEntity,
-    components,
+    components: {
+      ...result.components,
+      ...clientComponents,
+    },
   };
 }
