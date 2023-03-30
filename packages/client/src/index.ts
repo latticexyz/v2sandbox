83031f91ea3e589eb7b347198b1c71b34c6616cf
Kevin Ingersoll <kingersoll@gmail.com>
2023-03-30 08:11:45 -0700

    eoa works

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -3,15 +3,6 @@ import { setup } from "./mud/setup";
 
 const { components, networkConfig, network } = await setup();
 
-// Components expose a stream that triggers when the component is updated.
-components.CounterTable.update$.subscribe((update) => {
-  const [nextValue, prevValue] = update.value;
-  console.log("Counter updated", update, { nextValue, prevValue });
-  document.getElementById("counter")!.innerHTML = String(
-    nextValue?.value ?? "unset"
-  );
-});
-
 // Create a World contract instance
 const signer = network.signer.get();
 if (!signer) throw new Error("No signer");
@@ -23,8 +14,8 @@ const worldContract = IWorld__factory.connect(
 
 // Just for demonstration purposes: we create a global function that can be
 // called to invoke the Increment system contract via the world. (See IncrementSystem.sol.)
-(window as any).increment = async () => {
-  const txResult = await worldContract.mud_increment_increment({
+(window as any).spawn = async () => {
+  const txResult = await worldContract.spawn(5, 5, {
     gasLimit: 1_000_000,
     gasPrice: 0,
   });
