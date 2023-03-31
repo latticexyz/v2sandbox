ee9dbc68ecfad657a0d628aa95170607acf32195
Andy Cernera <kooshaba@gmail.com>
2023-03-31 14:06:16 -0400

    fix: switch back to singleton ID

--- packages/contracts/src/systems/IncrementSystem.sol
+++ packages/contracts/src/systems/IncrementSystem.sol
@@ -6,7 +6,7 @@ import { CounterTable } from "../tables/CounterTable.sol";
 
 contract IncrementSystem is System {
   function increment() public returns (uint32) {
-    bytes32 key = bytes32("singleton");
+    bytes32 key = bytes32(uint256(0x060D));
     uint32 counter = CounterTable.get(key);
     uint32 newValue = counter + 1;
     CounterTable.set(key, newValue);
