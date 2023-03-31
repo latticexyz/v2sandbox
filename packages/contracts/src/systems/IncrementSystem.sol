e2eb2f6c9ef994ff5ef0fced88ba39ea6de2a734
dk1a <dk1a@protonmail.com>
2023-03-31 23:12:06 +0300

    feat: getUniqueEntity integration test

--- packages/contracts/src/systems/IncrementSystem.sol
+++ packages/contracts/src/systems/IncrementSystem.sol
@@ -4,11 +4,12 @@ import { console } from "forge-std/console.sol";
 import { System } from "@latticexyz/world/src/System.sol";
 import { CounterTable } from "../tables/CounterTable.sol";
 
+import { getUniqueEntity } from "@latticexyz/world/src/modules/uniqueentity/getUniqueEntity.sol";
+
 contract IncrementSystem is System {
   function increment() public returns (uint32) {
     bytes32 key = bytes32(uint256(0x060D));
-    uint32 counter = CounterTable.get(key);
-    uint32 newValue = counter + 1;
+    uint32 newValue = uint32(uint256(getUniqueEntity()));
     CounterTable.set(key, newValue);
     return newValue;
   }
